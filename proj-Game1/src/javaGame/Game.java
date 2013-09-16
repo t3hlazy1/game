717918e09633928667d636fec45773674315a18a
t3hlazy1 <lilfootballplaya12@hotmail.com>
2013-09-16 00:07:35 -0500

    InitCommit

---
+++ proj-Game1/src/javaGame/Game.java
@@ -0,0 +1,36 @@
+package javaGame;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class Game extends StateBasedGame{
+  
+  public static final String gameName = "Java Game!";
+  public static final int menu = 0;
+  public static final int play = 1;
+  
+  public Game(String gameName){
+    super(gameName);
+    this.addState(new Menu(menu));
+    this.addState(new Play(play));
+  }
+  
+  public void initStatesList(GameContainer gc) throws  SlickException{
+    this.getState(menu).init(gc, this);
+    this.getState(play).init(gc, this);
+    this.enterState(menu);
+    
+  }
+  
+	public static void main(String[] args){
+	  AppGameContainer appGc;
+	  try{
+	    appGc = new AppGameContainer(new Game(gameName));
+	    appGc.setDisplayMode(500, 500, false);
+	    appGc.start();
+	  }catch(SlickException e){
+	    e.printStackTrace();
+	  }
+	}
+
+}
---
+++ proj-Game1/src/javaGame/Play.java
@@ -0,0 +1,102 @@
+package javaGame;
+
+import org.lwjgl.input.Mouse;
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class Play extends BasicGameState {
+int ammo = 30;
+int totalAmmo = 100;
+int shotDelta = 200;
+int posX = 250;
+int posY = 250;
+int movAmount = 1;
+long lastMovement = System.currentTimeMillis();
+long lastShot = System.currentTimeMillis();
+boolean mouseUp = true;
+boolean auto = false;
+boolean noAmmo = false;
+
+Image character = null;
+
+  public Play(int state){
+    
+  }
+  
+  public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
+    character = new Image("res/buckysFront.png");
+  }
+  
+  public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
+    
+    g.drawString("Ammo: " + ammo + "/" + totalAmmo, 10, 30);
+    g.drawString("" + System.currentTimeMillis(), 50, 50);
+    if (noAmmo){
+      g.drawString("No Ammo!", 300, 200);
+    }
+    character.draw(posX, posY);
+    g.drawLine(posX, posY+20, Mouse.getX(), 500-Mouse.getY());
+  }
+  
+  public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
+    
+    Input input = gc.getInput();
+    if(Mouse.isButtonDown(0) && ((long)(System.currentTimeMillis() - lastShot) > shotDelta) && (auto || mouseUp)){
+      if (ammo > 0){
+      ammo--;
+      }
+      lastShot = System.currentTimeMillis();
+      mouseUp = false;
+    }
+    if (Mouse.isButtonDown(0)){
+      mouseUp = false;
+    }
+    else{
+      mouseUp = true;
+    }
+    if (input.isKeyPressed(Input.KEY_R) || ammo == 0){
+      int ammoNeeded = 30 - ammo;
+      if (totalAmmo >= ammoNeeded){
+        totalAmmo -= ammoNeeded;
+        ammo += ammoNeeded;
+      }
+      else{
+        ammo += totalAmmo;
+        totalAmmo = 0;
+      }
+      if (ammo < 0){
+        ammo = 0;
+      }
+      if (totalAmmo < 0){
+        totalAmmo = 0;
+      }
+      if (ammo == 0 && totalAmmo == 0){
+        noAmmo = true;
+      }
+    }
+    if (System.currentTimeMillis() - lastMovement > 2){
+    if (input.isKeyDown(Input.KEY_UP) || input.isKeyDown(Input.KEY_W)){
+      posY -= movAmount;
+      character = new Image("res/buckysBack.png");
+    }
+    if (input.isKeyDown(Input.KEY_DOWN) || input.isKeyDown(Input.KEY_S)){
+      posY += movAmount;
+      character = new Image("res/buckysFront.png");
+    }
+    if (input.isKeyDown(Input.KEY_LEFT) || input.isKeyDown(Input.KEY_A)){
+      posX -= movAmount;
+      character = new Image("res/buckysLeft.png");
+    }
+    if (input.isKeyDown(Input.KEY_RIGHT) || input.isKeyDown(Input.KEY_D)){
+      posX += movAmount;
+      character = new Image("res/buckysRight.png");
+    }
+    lastMovement = System.currentTimeMillis();
+    }
+  }
+  
+  public int getID(){
+    return 1;
+  }
+  
+}
